6836f24251d70fb7178db8b2c5da41ff1e25aacf
Luis del Moral Martínez <ldmoral1987@gmail.com>
2023-10-23 21:04:21 +0200

    Se crean estilos adicionales y un diálogo de mensaje flotante para mostrar la carta seleccionada (adivinada). También se suprime la salida por consola utilizada para la depuración

--- scripts/game.js
+++ scripts/game.js
@@ -76,9 +76,6 @@ function selectCards()
     // Se comprueba que las cartas sean diferentes
     if ( (card1 != card2) && (card1 != card3) && (card1 != card4) && (card2 != card3) && (card2 != card4) && (card3 != card4))
     {
-        // Se muestra un mensaje con las cartas seleccionadas
-        alert("Cartas seleccionadas:\n" + card1 + "," + card2 + "," + card3 + "," + card4);
-
         // Ahora se tiene que calcular el orden que tienen las cartas para ver cuánto se sumas
         // Se consulta el vector de orden que contiene la ordenación maestra de todas las cartas
         // Para esto, se calculará el índice en el que se encuentra cada una de las cartas
@@ -87,9 +84,7 @@ function selectCards()
         idxCard3 = order.indexOf(card3);
         idxCard4 = order.indexOf(card4);
 
-        console.log(idxCard1);
-        console.log(idxCard3);
-        console.log(idxCard4);
+        // Esta variable almacenará la cantidad de cartas que tenemos que sumar
         var add = 0;
 
         // Con los índices, se puede determinar el orden en el que hay que sumar a la segunda carta
@@ -112,18 +107,36 @@ function selectCards()
             add = 5;
         else if ((idxCard1 > idxCard4) && (idxCard3 > idxCard4) && (idxCard1 > idxCard4))
             add = 6;
-
-        console.log(card2[0]);
-        console.log("Sumamos: " + add);
         
         // Se calcula la carta final (realizando el desplazamiento con la suma)
         // teniendo en cuenta el módulo 13 y añadiendo el palo de la carta al final
-        var answer = ((parseInt(card2[0]) + add) % 13) + types[document.getElementById("type2").value];
-        console.log("Tu carta es: " + answer);
+        var answer = types[document.getElementById("type2").value] + ((parseInt(card2[0]) + add) % 13);
+
+        // Se actualiza la carta del mensaje modal para mostrar la carta adivinada
+        document.getElementById("selectedCard").src = "./images/" + answer + ".png";
+
+        // Se muestra un mensaje modal con la carta escogida
+        showModal();
     }
     else
     {
         // Se han detectado cartas duplicadas
         alert("Error: no se permite utilizar cartas duplicadas");
     }
+}
+
+// Muestra la ventana modal
+function showModal()
+{
+    // Se obtiene la referencia al modal y se muestra
+    var modal = document.getElementById("modalMessage");
+    modal.style.display = "block";
+}
+
+// Cierra la ventana modal
+function closeModal()
+{
+    // Se obtiene la referencia al modal y se muestra
+    var modal = document.getElementById("modalMessage");
+    modal.style.display = "none";
 }
